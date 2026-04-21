ae2fd4626525fcbb710573def0c1885d26bc1719
ydhappy <94909904+ydhappy@users.noreply.github.com>
2026-04-21 12:07:04 +0900

    feat: add policies package init

---
+++ app/policies/__init__.py
@@ -0,0 +1,11 @@
+from app.policies.collector_policy import collector_policy
+from app.policies.dealer_policy import dealer_policy
+from app.policies.healer_policy import healer_policy
+from app.policies.tank_policy import tank_policy
+
+ROLE_POLICIES = [
+    healer_policy,
+    tank_policy,
+    collector_policy,
+    dealer_policy,
+]
