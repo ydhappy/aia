e9f511dce2472e9e4ea3476c67da7f639c80050e
ydhappy <94909904+ydhappy@users.noreply.github.com>
2026-04-21 12:25:47 +0900

    feat: register support and scout policies

--- app/policies/__init__.py
+++ app/policies/__init__.py
@@ -1,6 +1,8 @@
 from app.policies.collector_policy import collector_policy
 from app.policies.dealer_policy import dealer_policy
 from app.policies.healer_policy import healer_policy
+from app.policies.scout_policy import scout_policy
+from app.policies.support_policy import support_policy
 from app.policies.tank_policy import tank_policy
 
 ROLE_POLICIES = [
@@ -8,4 +10,6 @@ ROLE_POLICIES = [
     tank_policy,
     collector_policy,
     dealer_policy,
+    support_policy,
+    scout_policy,
 ]
